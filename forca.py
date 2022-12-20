9c163475b74cd5ce6476dfa8420704ac022cef42
PizzaModaDaCasa <87745411+AlanPizza@users.noreply.github.com>
2022-12-20 09:43:34 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,26 +1,189 @@
-def jogar():
-    print('************************************************************************')
-    print('Bem vindo ao jogo da forca')
-    print('************************************************************************')
+import random
+import mr_robot
 
-    palavra_secreta = 'cachorro'
+def forca():
+    imprime_mensagem_abertura()
+
+    palavra_secreta = carrega_palavra_secreta()
+
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
+    erros = 0
 
-    
     while(not enforcou and not acertou):
 
-        chute = input('Qual letra? ')
+        chute = pede_chute()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute == letra):
-                print(f'Encontrei a letra {letra} na posição {index}')
-            index = index + 1
+        if(chute in palavra_secreta):
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
 
-        print('iniciando...')
+        else:
+            erros += 1
+            desenha_forca(erros)
+
+        enforcou = erros == 7
+        acertou = '_' not in letras_acertadas
+        print(letras_acertadas)
+
+    if (acertou):
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor(palavra_secreta)
+
+def imprime_mensagem_abertura():
+    print('************************************************************************')
+    print('Bem vindo ao jogo da forca, a temática desse jogo é FRUTAS, e você tem\n7 tentativas para descobrir a palavra secreta. Boa sorte! :)')
+    print('************************************************************************')
+
+def carrega_palavra_secreta():
+    arquivo = open('meus_projetos/palavras.txt','r')
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    return palavra_secreta
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+def pede_chute():
+    chute = input('Qual letra? ')
+    chute = chute.strip().upper()
+
+    return chute
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+    print('\n')
+    print('Escolha uma das opções abaixo para prosseguirmos.')
+    print('(1) Jogar novamente')
+    print('(2) Voltar as minhas pastas de projetos')
+    print('(#) Ou digite qualquer outra coisa para SAIR.')
+
+    opcao = str(input('Digite o que você deseja: '))
+
+    if (opcao == '1'):
+        print('\n' * 130)
+        forca()
+    elif (opcao == '2'):
+        print('\n' * 130)
+        mr_robot.robot()
+    else:
+        print('Até mais ;)')
+
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Não foi dessa vez...")
+    print(f"A palavra era {palavra_secreta}")
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+    print('\n')
+    print('Escolha uma das opções abaixo para prosseguirmos.')
+    print('(1) Jogar novamente')
+    print('(2) Voltar as minhas pastas de projetos')
+    print('(#) Ou digite qualquer outra coisa para SAIR.')
+
+    opcao = str(input('Digite o que você deseja: '))
+
+    if (opcao == '1'):
+        print('\n' * 130)
+        forca()
+    elif (opcao == '2'):
+        print('\n' * 130)
+        mr_robot.robot()
+    else:
+        print('Até mais ;)')
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 if (__name__ == '__main__'):
-    jogar()
+    forca()
--- mr_robot.py
+++ mr_robot.py
@@ -1,26 +1,31 @@
-import projeto_adivinhacao
+import adivinhacao
+import forca
 
 def robot():
-    print('\n' * 130)
-    print('************************************************************************')
-    print('Olá Mr. Robot, aqui é o seu espaço de projetos feitos ou em andamentos.')
-    print('************************************************************************')
-    print('Aqui está os seguintes projetos:')
-    print('(1) Adivinhacao')
-    print('(2) Forca')
-    print('Caso você deseje sair basta digitar SAIR em maiúsculo ou digitar 0')
-    print('************************************************************************')
     
     while True:
+        print('\n' * 130)
+        print('************************************************************************')
+        print('Olá Mr. Robot, aqui é o seu espaço de projetos feitos ou em andamentos.')
+        print('************************************************************************')
+        print('Aqui está os seguintes projetos:')
+        print('(1) Adivinhacao')
+        print('(2) Forca')
+        print('Caso você deseje sair basta digitar SAIR em maiúsculo ou digitar 0')
+        print('************************************************************************')
         projeto = str(input('Qual deles você deseja executar? '))
+        
         if (projeto == '1'):
             print('\n' * 130)
-            print('**Executando o projeto "Adivinhacao"**')
-            projeto_adivinhacao.adivinhacao()
+            print('**Executando "Adivinhacao"**')
+            adivinhacao.adivinhacao()
             break
 
         elif (projeto == '2'):
-            print('Me desculpe, mas este projeto ainda está em andamento...')
+            print('\n' *130)
+            print('**Executando "Forca"**')
+            forca.forca()
+
             continue
 
         elif (projeto == '0') or (projeto == 'SAIR'):
